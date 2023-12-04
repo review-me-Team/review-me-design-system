40433452a9c155a6efdac7751368dfb31a34ba2f
kinsk2839 <kinsk2839@naver.com>
2023-12-04 19:16:25 +0900

    feat: GlobalStyle 생성 (#4)

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    font: inherit;
+    vertical-align: baseline;
+  }
+  html, body {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+  article, aside, details, footer, header, menu, nav, section {
+    display: block;
+  }
+  ol, ul {
+    list-style: none;
+  }
+`;
