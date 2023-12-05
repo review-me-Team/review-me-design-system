16360072abe871272647ad5d9633b2c08bdc9e0b
kinsk2839 <kinsk2839@naver.com>
2023-12-05 21:12:37 +0900

    feat: storybook에 쓰일 style 생성 (#3)
    - prop에 따라 다른 UI를 보여주기 위해 스타일 생성

---
+++ src/styles/storyStyle.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ComponentList = styled.ul<{ direction: 'row' | 'column' }>`
+  display: flex;
+  flex-direction: ${({ direction }) =>
+    direction === 'row' || direction === 'column' ? direction : 'column'};
+  gap: 24px;
+`;
+export const ComponentListItem = styled.li<{ direction: 'row' | 'column' }>`
+  display: flex;
+  flex-direction: ${({ direction }) =>
+    direction === 'row' || direction === 'column' ? direction : 'column'};
+  width: fit-content;
+  gap: 8px;
+`;
