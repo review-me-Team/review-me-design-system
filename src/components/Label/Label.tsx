d2f6a153ab382cc31f814a188aaf103f8a779a5e
kinsk2839 <kinsk2839@naver.com>
2023-12-07 17:51:38 +0900

    feat: Label 컴포넌트의 prop으로 children 추가 (#7)

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { LabelLayout } from './style';
 
 interface Props {
   isActive: boolean;
+  children: ReactNode;
   px?: number;
   py?: number;
 }
 
-const Label = ({ isActive, px = 20, py = 8 }: Props) => {
+const Label = ({ isActive, children, px = 20, py = 8 }: Props) => {
   return (
     <LabelLayout $isActive={isActive} $px={px} $py={py}>
-      Label
+      {children}
     </LabelLayout>
   );
 };
--- src/stories/Label.stories.tsx
+++ src/stories/Label.stories.tsx
@@ -11,6 +11,10 @@ export default {
       description: '라벨의 활성화를 나타냅니다.',
       control: 'boolean',
     },
+    children: {
+      description: '라벨에 들어갈 컨텐츠를 표시합니다.',
+      control: 'text',
+    },
     px: {
       description: '라벨의 padding-left, padding-right 값을 지정합니다',
       control: 'number',
@@ -22,6 +26,7 @@ export default {
   },
   args: {
     isActive: false,
+    children: 'Label',
   },
 } satisfies Meta<typeof Label>;
 
@@ -30,5 +35,6 @@ type Story = StoryObj<typeof Label>;
 export const Default: Story = {
   args: {
     isActive: false,
+    children: 'Label',
   },
 };
