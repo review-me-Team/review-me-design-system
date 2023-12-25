9abed82aae115046ff953d01e6366dc430ad95bf
aken-you <kinsk2839@naver.com>
2023-12-25 22:01:17 +0900

    feat: Modal 컴포넌트 생성 (#27)

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,43 @@
+import React, { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+import { BackDrop, Description, ModalLayout, Title } from './style';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  modalRootId: string;
+  children: ReactNode;
+  width?: string;
+  height?: string;
+  padding?: string;
+}
+
+const Modal = ({
+  isOpen,
+  onClose,
+  modalRootId,
+  children,
+  width = 'fit-content',
+  height = 'fit-content',
+  padding = '1.25rem',
+}: Props) => {
+  return createPortal(
+    <>
+      {isOpen && (
+        <>
+          <ModalLayout $width={width} $height={height} $padding={padding}>
+            {children}
+          </ModalLayout>
+          <BackDrop onClick={onClose} />
+        </>
+      )}
+    </>,
+    document.getElementById(modalRootId) as HTMLElement,
+  );
+};
+
+Modal.Title = Title;
+Modal.Description = Description;
+
+export default Modal;
