2f9f5110dcd76c9d9afdfb03627dd4640a059f2b
aken-you <kinsk2839@naver.com>
2023-12-25 22:01:33 +0900

    feat: Modal storybook 작성 (#27)

---
+++ src/stories/Modal.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import React from 'react';
+
+import useModal from '@hooks/useModal';
+
+import Button from '@components/Button/Button';
+import Modal from '@components/Modal/Modal';
+
+export default {
+  title: 'Modal',
+  component: Modal,
+  tags: ['autodocs'],
+  argTypes: {
+    isOpen: {
+      description: '모달이 현재 표시되고 있는지 여부를 나타냅니다.',
+    },
+    modalRootId: {
+      description: '모달이 위치할 element의 id를 적어주세요.',
+    },
+    children: {
+      description: '모달의 컨텐츠를 설정합니다.',
+    },
+    width: {
+      description: '모달의 가로 길이를 설정합니다. (기본값은 content에 맞게 설정)',
+    },
+    height: {
+      description: '모달의 세로 길이를 설정합니다. (기본값은 content에 맞게 설정)',
+    },
+    padding: {
+      description: '모달의 padding을 설정합니다. (기본값은 1.25rem)',
+    },
+  },
+} as Meta<typeof Modal>;
+
+type Story = StoryObj<typeof Modal>;
+
+export const Default: Story = {
+  argTypes: {
+    modalRootId: { control: { disable: true } },
+  },
+  render: ({ width, height, padding }) => {
+    const { isOpen, open, close } = useModal();
+
+    return (
+      <>
+        <Button variant="default" size="m" onClick={open}>
+          open
+        </Button>
+        <Modal
+          isOpen={isOpen}
+          onClose={close}
+          modalRootId="modal-root"
+          width={width}
+          height={height}
+          padding={padding}
+        >
+          <div>
+            <Modal.Title>title</Modal.Title>
+            <Modal.Description>description</Modal.Description>
+          </div>
+        </Modal>
+      </>
+    );
+  },
+};
