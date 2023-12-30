0f8c54046e10c161ec8ad3581c7a83683ab628b8
aken-you <kinsk2839@naver.com>
2023-12-30 14:29:38 +0900

    feat: option select handler 추가 및 검색 기능 추가 (#29)

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import Icon from '@components/Icon/Icon';
+import Input from '@components/Input/Input';
 
 import { OptionItem, OptionList, SelectLayout, SelectedValue, TriggerButton } from './style';
 
@@ -12,29 +13,55 @@ interface Option {
 interface Props {
   options: Option[];
   defaultOption?: Option;
+  onSelectOption: (option?: Option) => void;
+  isSearch?: boolean;
   width?: string;
   maxHeight?: string;
 }
 
-const Select = ({ options, defaultOption, width = '100%', maxHeight }: Props) => {
+const Select = ({
+  options,
+  defaultOption,
+  onSelectOption,
+  isSearch = false,
+  width = '100%',
+  maxHeight,
+}: Props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState<Option | undefined>(defaultOption);
+  const [typedValue, setTypedValue] = useState<string | null>(null);
+
+  const filteredOptions = typedValue
+    ? options.filter((option) => option.value.startsWith(typedValue))
+    : options;
 
   return (
     <SelectLayout $width={width}>
-      <TriggerButton onClick={() => setIsOpen((prev) => !prev)}>
-        <SelectedValue>{selectedOption?.value ?? ''}</SelectedValue>
-        {isOpen ? <Icon iconName="upArrow" /> : <Icon iconName="downArrow" />}
-      </TriggerButton>
+      {isSearch ? (
+        <Input
+          value={typedValue ?? selectedOption?.value ?? ''}
+          onFocus={() => setIsOpen(true)}
+          onBlur={() => {
+            setIsOpen(false);
+          }}
+          onChange={({ target }) => setTypedValue(target.value)}
+        />
+      ) : (
+        <TriggerButton onClick={() => setIsOpen((prev) => !prev)}>
+          <SelectedValue>{selectedOption?.value ?? ''}</SelectedValue>
+          {isOpen ? <Icon iconName="upArrow" /> : <Icon iconName="downArrow" />}
+        </TriggerButton>
+      )}
 
       {isOpen && (
         <OptionList onClick={() => setIsOpen(false)} maxHeight={maxHeight}>
-          {options.map((option) => (
+          {filteredOptions.map((option) => (
             <OptionItem
               key={option.id}
               $isSelected={selectedOption?.id === option.id}
-              onClick={() => {
+              onMouseDown={() => {
                 setSelectedOption(option);
+                onSelectOption(option);
               }}
             >
               {option.value}
--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -13,6 +13,9 @@ export default {
     defaultOption: {
       description: '기본값으로 할 option을 작성합니다. (option은 id와 value로 이루어진 객체입니다.)',
     },
+    onSelectOption: {
+      description: 'option을 선택했을 때 실행되는 handler입니다.',
+    },
   },
 } as Meta<typeof Select>;
 
@@ -25,6 +28,7 @@ export const Default: Story = {
       { id: 'graph', value: '포도' },
       { id: 'banana', value: '바나나' },
     ],
+    onSelectOption: (option) => console.log(option),
   },
 };
 
@@ -44,5 +48,26 @@ export const DefaultOption: Story = {
       { id: 'banana', value: '바나나' },
     ],
     defaultOption: { id: 'apple', value: '사과' },
+    onSelectOption: (option) => console.log(option),
+  },
+};
+
+export const SearchInput: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          '기본 option이 있는 경우 trigger에 기본값이 표시됩니다. 이 때, option은 id와 value로 이뤄진 객체로 작성해주세요. ',
+      },
+    },
+  },
+  args: {
+    options: [
+      { id: 'apple', value: '사과' },
+      { id: 'graph', value: '포도' },
+      { id: 'banana', value: '바나나' },
+    ],
+    isSearch: true,
+    onSelectOption: (option) => console.log(option),
   },
 };
