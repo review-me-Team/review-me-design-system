67212dea96384b8f9bb250d1a3fee781eae11b6b
aken-you <kinsk2839@naver.com>
2024-01-02 16:23:54 +0900

    refactor: Select storybook 설명 추가 (#29)

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -11,15 +11,25 @@ export default {
   argTypes: {
     defaultOption: {
       description: '기본으로 선택된 option입니다. (label: option의 text, value: option의 value)',
+      control: 'object',
     },
     onSelectOption: {
       description: 'option을 선택했을 때의 handler입니다.',
     },
     width: {
       description: 'Select 컴포넌트의 가로 길이 입니다.',
+      control: 'text',
     },
     children: {
-      description: 'Select 컴포넌트에 들어갈 content를 넣어줍니다.',
+      component: 'Select 컴포넌트에 들어갈 content를 넣어줍니다.',
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Select 컴포넌트는 Compound Component Pattern을 적용합니다. `<Select.TriggerButton>` 혹은 `<Select.TriggerInput>` 다음에 `<Select.OptionList>`를 선언해주세요.\n- `<Select.TriggerButton>`은 option 목록을 여닫는 버튼 컴포넌트입니다.\n- `<Select.TriggerInput>`은 입력한 검색어에 해당하는 option 목록을 보여주는 input 컴포넌트입니다.\n- `<Select.OptionList>`는 option 목록에 해당하는 컴포넌트입니다. maxHeight prop을 통해 높이를 정할 수 있습니다.\n- `<Select.OptionItem>`은 option에 해당하는 컴포넌트입니다. prop으로 value에 option의 value를, label에 option의 text를 입력해주세요.',
+      },
     },
   },
 } as Meta<typeof Select>;
