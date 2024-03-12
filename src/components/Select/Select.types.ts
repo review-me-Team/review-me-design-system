cc55655c48cec9504af4870663581b79f0a94a00
aken-you <kinsk2839@naver.com>
2024-03-12 18:20:05 +0900

    refactor: Name 타입 빼기 (#66)

--- src/components/Select/Select.types.ts
+++ src/components/Select/Select.types.ts
@@ -1,13 +1,8 @@
 /**
- * Name: option에 표시할 text
- */
-export type Name = number | string;
-/**
  * Value: option의 value
  */
 export type Value = number | string;
 
 export interface Option {
   value: Value;
-  name: Name;
 }
