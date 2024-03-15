2f9dad0e2a2eb7030675324e0ce6f79f65b5cc99
aken-you <kinsk2839@naver.com>
2024-03-15 11:30:58 +0900

    feat: MultiRangeSlider storybook에 새로 추가된 prop 반영 (#77)

--- src/stories/MultiRangeSlider.stories.tsx
+++ src/stories/MultiRangeSlider.stories.tsx
@@ -29,6 +29,14 @@ const meta: Meta<typeof MultiRangeSlider> = {
       description: '슬라이더의 범위를 설정합니다. 범위의 최솟값과 최댓값을 객체로 설정합니다.',
       control: 'object',
     },
+    hasGreaterCheck: {
+      description: '범위의 최댓값과 max가 동일하면, + 표시할건지 여부를 설정합니다.',
+      control: 'boolean',
+    },
+    hasLessCheck: {
+      description: '범위의 최솟값과 min이 동일하면, - 표시할건지 여부를 설정합니다.',
+      control: 'boolean',
+    },
   },
 };
 
@@ -37,11 +45,20 @@ export default meta;
 type Story = StoryObj<typeof MultiRangeSlider>;
 
 export const Default: Story = {
-  render: ({ width, step }) => {
+  render: ({ width, step, hasGreaterCheck, hasLessCheck }) => {
     const [range, setRange] = useState({ min: 0, max: 10 });
 
     return (
-      <MultiRangeSlider width={width} step={step} min={0} max={10} range={range} onRangeChange={setRange} />
+      <MultiRangeSlider
+        width={width}
+        step={step}
+        min={0}
+        max={10}
+        range={range}
+        onRangeChange={setRange}
+        hasGreaterCheck={hasGreaterCheck}
+        hasLessCheck={hasLessCheck}
+      />
     );
   },
 };
