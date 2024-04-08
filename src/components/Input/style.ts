76046c11e8669568ddb51fa802f0b120c3d2441f
aken-you <kinsk2839@naver.com>
2024-04-08 16:46:54 +0900

    refactor: Input 컴포넌트에 focus-visible 스타일 제거 (#99)

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -19,9 +19,6 @@ const StyledInput = styled.input<{ $isValid: boolean }>`
     border: 1px solid ${({ theme }) => theme.color.accent.bd.strong};
     color: ${({ theme }) => theme.color.neutral.text.default};
   }
-  &:focus-visible {
-    outline: none;
-  }
 `;
 
 export { StyledInput };
