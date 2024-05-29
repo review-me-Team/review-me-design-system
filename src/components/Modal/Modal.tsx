59ea377aa816c7c0c4b640ce91113c8f615a66a0
aken-you <kinsk2839@naver.com>
2024-05-29 13:49:01 +0900

    refactor: Modal 컴포넌트가 브라우저 창보다 클 경우, BackDrop에 스크롤 생성 (#106)

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,7 @@
-import React, { ComponentPropsWithoutRef } from 'react';
+import React, { ComponentPropsWithoutRef, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-import { BackDrop, Description, ModalLayout, Title } from './style';
+import { BackDrop, Description, ModalLayout, ModalWrapper, Title } from './style';
 
 interface Props extends ComponentPropsWithoutRef<'div'> {
   isOpen: boolean;
@@ -21,15 +21,24 @@ const Modal = ({
   height = 'fit-content',
   padding = '1.25rem',
 }: Props) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
   if (!isOpen) return null;
 
   return createPortal(
-    <>
-      <ModalLayout $width={width} $height={height} $padding={padding}>
-        {children}
-      </ModalLayout>
-      <BackDrop onClick={onClose} />
-    </>,
+    <BackDrop
+      onClick={(e) => {
+        if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+          onClose(e);
+        }
+      }}
+    >
+      <ModalWrapper>
+        <ModalLayout ref={modalRef} $width={width} $height={height} $padding={padding}>
+          {children}
+        </ModalLayout>
+      </ModalWrapper>
+    </BackDrop>,
     document.getElementById(modalRootId) as HTMLElement,
   );
 };
