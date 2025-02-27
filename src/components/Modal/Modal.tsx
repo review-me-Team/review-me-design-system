3a870668ada82baa7cb619110eda01a80a79dc9b
kinsk2839 <96980857+aken-you@users.noreply.github.com>
2025-02-28 00:03:50 +0900

    Revert "[Feat] modal 컴포넌트에 focus trap 설정"

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,11 +1,11 @@
-import React, { ComponentPropsWithoutRef, useEffect, useRef } from 'react';
+import React, { ComponentPropsWithoutRef, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 import { BackDrop, Description, ModalLayout, ModalWrapper, Title } from './style';
 
 interface Props extends ComponentPropsWithoutRef<'div'> {
   isOpen: boolean;
-  onClose: () => void;
+  onClose: React.MouseEventHandler<HTMLElement>;
   modalRootId?: string;
   width?: string;
   height?: string;
@@ -23,85 +23,18 @@ const Modal = ({
 }: Props) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (!isOpen || !modalRef.current) return;
-
-    const modalElement = modalRef.current as HTMLDivElement;
-
-    // focus가 가능한 요소들 모두 찾기
-    const focusableElements = modalElement.querySelectorAll(
-      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])',
-    );
-
-    /**
-     * @description focus가 가능한 요소들 중 가장 첫번째, 마지막 요소 찾기
-     * focus가 가능한 요소가 없을 경우 modalElement에 focus
-     */
-    const firstElement =
-      focusableElements.length === 0 ? modalElement : (focusableElements[0] as HTMLElement);
-    const lastElement =
-      focusableElements.length === 0
-        ? modalElement
-        : (focusableElements[focusableElements.length - 1] as HTMLElement);
-
-    firstElement.focus();
-
-    const handleTabKeyPress = (event: KeyboardEvent) => {
-      if (event.key !== 'Tab') return;
-
-      // document.activeElement는 현재 focus가 있는 요소를 참조
-      // 모달 내 첫 번째 포커스 가능한 요소에 포커스된 상태에서 shift + tab 키를 누르면 마지막 포커스 가능한 요소로 이동
-      if (event.shiftKey && document.activeElement === firstElement) {
-        event.preventDefault();
-        lastElement.focus();
-        return;
-      }
-
-      // 모달 내 마지막 포커스 가능한 요소에 포커스된 상태에서 tab 키를 누르면 첫 번째 포커스 가능한 요소로 이동
-      if (!event.shiftKey && document.activeElement === lastElement) {
-        event.preventDefault();
-        firstElement.focus();
-      }
-    };
-
-    const handleEscapeKeyPress = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
-        onClose();
-      }
-    };
-
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === 'Tab') handleTabKeyPress(event);
-      if (event.key === 'Escape') handleEscapeKeyPress(event);
-    };
-
-    modalElement.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      modalElement.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [isOpen, onClose]);
-
   if (!isOpen) return null;
 
   return createPortal(
     <BackDrop
       onClick={(e) => {
         if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
-          onClose();
+          onClose(e);
         }
       }}
     >
       <ModalWrapper>
-        <ModalLayout
-          ref={modalRef}
-          $width={width}
-          $height={height}
-          $padding={padding}
-          tabIndex={-1}
-          role="dialog"
-          aria-modal={isOpen}
-        >
+        <ModalLayout ref={modalRef} $width={width} $height={height} $padding={padding}>
           {children}
         </ModalLayout>
       </ModalWrapper>
