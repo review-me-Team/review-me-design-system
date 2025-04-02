17b7a9a8b85c3250165f913b24f8c631ed6f96ad
aken-you <kinsk2839@naver.com>
2025-04-02 16:01:22 +0900

    chore: tsc-alias 삭제

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -20,14 +20,9 @@ const config: StorybookConfig = {
     if (config.resolve) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        '@pages': path.resolve(__dirname, '../src/pages'),
         '@components': path.resolve(__dirname, '../src/components'),
         '@assets': path.resolve(__dirname, '../src/assets'),
         '@styles': path.resolve(__dirname, '../src/styles'),
-        '@contexts': path.resolve(__dirname, '../src/contexts'),
-        '@hooks': path.resolve(__dirname, '../src/hooks'),
-        '@utils': path.resolve(__dirname, '../src/utils'),
-        '@constants': path.resolve(__dirname, '../src/constants'),
       };
 
       if (config.module?.rules) {
--- webpack.common.js
+++ webpack.common.js
@@ -30,14 +30,9 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json', '.svg'],
     alias: {
-      '@pages': path.resolve(__dirname, 'src/pages'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@styles': path.resolve(__dirname, 'src/styles'),
-      '@contexts': path.resolve(__dirname, 'src/contexts'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@constants': path.resolve(__dirname, 'src/constants'),
       '@stories': path.resolve(__dirname, 'src/stories'),
     },
   },
